40f1ec76c217f2f1efad7bdedae937ba1e59a78f
Vincent Samuel <chintuvincent@gmail.com>
2024-07-17 14:22:05 +0530

    fixed bug

--- email.js
+++ email.js
@@ -7,44 +7,51 @@ require("dotenv").config();
 const port = 5000;
 const app = express();
 app.use(bodyParser.json());
-app.use(cors());
 
+// Configure CORS
+const corsOptions = {
+  origin: 'https://portfolio-two-lake-84.vercel.app', // Allow your frontend domain
+  methods: 'GET,POST', // Allow these methods
+  allowedHeaders: 'Content-Type,Authorization', // Allow these headers
+};
+
+app.use(cors(corsOptions));
 
 app.post("/send", (req, res) => {
-const {fullName, email, contact, message} = req.body
+  const { fullName, email, contact, message } = req.body;
 
-const transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     service: 'gmail',
-    host:"smtp.gmail.com",
-    port:587,
+    host: "smtp.gmail.com",
+    port: 587,
     secure: false,
-    auth:{
-        user: process.env.USER,
-        pass: process.env.PASS,
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
     }
-})
+  });
 
-const mailOptions = {
+  const mailOptions = {
     from: `${email}`,
     to: "vincentkesari@gmail.com",
     subject: `Portfolio contact from ${fullName}`,
     text: `Name: ${fullName}\nEmail: ${email}\nContact: ${contact}\nMessage: ${message}`,
   };
 
-
-const sendMail = async (transporter, mailOptions) => {
-    try{
-        await transporter.sendMail(mailOptions);
-        console.log("email has been sent")
+  const sendMail = async (transporter, mailOptions) => {
+    try {
+      await transporter.sendMail(mailOptions);
+      console.log("Email has been sent");
+      res.status(200).send("Email sent successfully");
     } catch (error) {
-        console.log(error);
+      console.log(error);
+      res.status(500).send("Error sending email");
     }
-}  
+  };
 
-sendMail(transporter, mailOptions)
+  sendMail(transporter, mailOptions);
 });
 
-
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+  console.log(`Server running at http://localhost:${port}`);
+});
