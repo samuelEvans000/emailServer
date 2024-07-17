c7ec1d961d4d16e04e2e28d84b62c8d98d89403d
Vincent Samuel <chintuvincent@gmail.com>
2024-07-17 15:05:43 +0530

    fixed bug

--- email.js
+++ email.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://portfolio-two-lake-84.vercel.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
